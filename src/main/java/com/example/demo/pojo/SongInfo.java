983d8317949069c939692fa9c6ff40fc413ae8e1
Song <56920769+yifachen521@users.noreply.github.com>
2022-04-28 13:41:48 +0800

    commit

---
+++ src/main/java/com/example/demo/pojo/SongInfo.java
@@ -0,0 +1,18 @@
+package com.example.demo.pojo;
+
+
+import org.springframework.stereotype.Component;
+
+/**
+ * @description
+ * @Author QianWei.Song
+ * @Date: 2022/2/28 10:32
+ * @Version 1.1.0
+ */
+@Component
+public class SongInfo {
+
+    String name;
+    String address;
+    String wife;
+}
